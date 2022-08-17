0bd832a0cf2fdadd9dda1849f80201bb4d5aef22
Eniola Ademola <it.eniolaademola@gmail.com>
2022-08-17 15:23:55 +0100

    Update index.js to createroute latest react rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { ReactDOM } from "react-dom";
-import App from "./componentsApp.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
